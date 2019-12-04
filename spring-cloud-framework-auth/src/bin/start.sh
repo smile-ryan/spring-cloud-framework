fd0aa762587c147adc8056c6afc50a7e18883bfe
ryan <chenrui@yto.net.cn>
2019-12-04 17:58:44 +0800

    update

--- spring-cloud-framework-auth/src/bin/start.sh
+++ spring-cloud-framework-auth/src/bin/start.sh
@@ -10,4 +10,3 @@ JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow"
 
 echo "######################### APPLICATION STARTING #########################"
 nohup java -server ${JAVA_OPT} -jar ${APP_DIR}/boot/${APP_JAR_NAME} --spring.config.location=${APP_DIR}/conf/ > /dev/null 2>&1 &
-echo "######################### APPLICATION RUNNING #########################"
